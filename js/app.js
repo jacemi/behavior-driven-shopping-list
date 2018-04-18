018a47696e9c054e330dcb4f1ccdbd15898a9999
Jace Mitsuka <PJace@BigMac.local>
2018-04-18 10:01:17 -1000

    next

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let todaysList = new ShoppingList();
+let listContent = todaysList.render();
+content.innerHTML = listContent; 
+
+function add_to_shopping_list() {
+  let new_shopping_list_item = new ShoppingListItem(title.value, description.value);
+  todaysList.addItem(new_shopping_list_item);
+  let listContent = todaysList.render();
+  content.innerHTML = listContent
+  
+}
+
