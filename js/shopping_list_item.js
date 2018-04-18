0004308f776fb28d145234f6b913c3220dd74f5a
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-17 16:30:05 -1000

    shopping list items done

---
+++ js/shopping_list_item.js
@@ -0,0 +1,30 @@
+class ShoppingListItem {
+  constructor(name, description) {
+    this.name = name;
+    if (typeof (this.name) !== 'string') {
+      throw new Error('please use a string for name parameter');
+    }
+    this.description = description;
+    if (typeof (this.description) !== 'string') {
+      throw new Error('please use a string for description parameter');
+    }
+    this.is_done = false;
+  }
+
+  check(){
+    this.is_done = true;
+  }
+
+  uncheck(){
+    this.is_done = false;
+  }
+
+  render(){
+    return `<ul>
+  <li class="completed_${this.is_done}">
+    <span>${this.name}</span>
+    <span>${this.description}</span>
+  </li>
+<ul>`
+  }
+};
--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -6,16 +6,14 @@ describe('ShoppingListItem', function () {
     ShoppingListItem.should.be.a('function');
   });
   it('should have a property named name', function () {
-    var milk = new ShoppingListItem('milk');
-    milk.name.should.equal('milk');
-    var number = new ShoppingListItem(4);
-    expect.number.to.be.false;
+    var milk = new ShoppingListItem('milk', '');
+    milk.name.should.equal('milk', '');
+    expect(milk.name).to.be.a('string');
   });
   it('should have a property named description', function () {
     var milk = new ShoppingListItem('milk', 'comes from cows');
     milk.description.should.equal('comes from cows');
-    var grapes = new ShoppingListItem('grapes', 34);
-    expect.grapes.to.be.false;
+    expect(milk.description).to.be.a('string');
   });
   it('should have a property named is_done', function () {
     var milk = new ShoppingListItem('milk', 'comes from cows');
@@ -32,7 +30,7 @@ describe('ShoppingListItem', function () {
 describe('.check', function () {
   it('should be a function', function () {
     var milk = new ShoppingListItem('milk', 'comes from cows');
-    expect(milk.check).should.be.a('function');
+    expect(milk.check).to.be.a('function');
   });
   it('.check should set is_done property to true', function () {
     var milk = new ShoppingListItem('milk', 'comes from cows');
@@ -44,7 +42,7 @@ describe('.check', function () {
 describe('.uncheck', function () {
   it('should be a function', function () {
     var milk = new ShoppingListItem('milk', 'comes from cows');
-    expect(milk.uncheck).should.be.a('function');
+    expect(milk.uncheck).to.be.a('function');
   });
   it('.uncheck should set is_done property to false', function () {
     var milk = new ShoppingListItem('milk', 'comes from cows');
@@ -56,12 +54,15 @@ describe('.uncheck', function () {
 describe('.render', function () {
   it('should be a function', function () {
     var milk = new ShoppingListItem('milk', 'comes from cows');
-    expect(milk.render).to.equal(`<ul>
+    expect(milk.render).to.be.a('function');
+  });
+  it("calling the instance's render method will construct and return an html formatted string", function () {
+    var milk = new ShoppingListItem('milk', 'comes from cows');
+    expect(milk.render()).to.equal(`<ul>
   <li class="completed_false">
     <span>milk</span>
     <span>comes from cows</span>
-  </li> 
+  </li>
 <ul>`);
   });
 });
-
